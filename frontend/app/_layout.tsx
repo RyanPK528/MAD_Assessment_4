658296713ea0b20e3371d9b7fb03fab79f2cccbb
TanaRuin <vammyjiang07@gmail.com>
2026-06-01 05:30:16 +0700

    update: layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import { ThemeProvider } from '@/components/ThemeContext';
+import { ensureSyncQueueInitialized, syncPendingResults } from '@/services/activityResultService';
 
 export default function RootLayout() {
+  useEffect(() => {
+    void ensureSyncQueueInitialized().then(() => syncPendingResults());
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
